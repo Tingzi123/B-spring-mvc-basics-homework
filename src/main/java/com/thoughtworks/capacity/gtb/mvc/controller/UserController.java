3ef6e8f635eff5f4a31da69908ee9a1ee6dd7670
Tingzi123 <1044373556@qq.com>
2020-11-04 00:44:30 +0800

    [ct]:fix log validated

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/UserController.java
@@ -3,14 +3,19 @@ package com.thoughtworks.capacity.gtb.mvc.controller;
 import com.thoughtworks.capacity.gtb.mvc.dto.User;
 import com.thoughtworks.capacity.gtb.mvc.exception.UserExistsException;
 import com.thoughtworks.capacity.gtb.mvc.service.UserService;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @RestController
+@Validated
 public class UserController {
     @Autowired
     public UserService userService;
@@ -27,7 +32,13 @@ public class UserController {
     }
 
     @GetMapping(value = "/login")
-    public ResponseEntity<User> login(@RequestParam String username, @RequestParam String password) {
+    public ResponseEntity<User> login(@RequestParam
+                                      @NotBlank
+                                      @Pattern(regexp = "[0-9A-Za-z_]{3,10}")
+                                      @Length(min = 3, max = 10) String username,
+                                      @RequestParam
+                                      @NotBlank
+                                      @Length(min = 5, max = 12) String password) {
         User user = userService.login(username, password);
 
         return ResponseEntity.ok(user);
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/exception/GlobalExceptionHandler.java
@@ -6,6 +6,10 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import java.util.Set;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
@@ -23,6 +27,19 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResult> handle(MethodArgumentNotValidException ex) {
         String message = ex.getBindingResult().getFieldError().getDefaultMessage();
+        ErrorResult errorResult = new ErrorResult(message);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResult> handle(ConstraintViolationException ex) {
+        Set<ConstraintViolation<?>> violations = ex.getConstraintViolations();
+
+        String message = "";
+        for (ConstraintViolation<?> constraint : ex.getConstraintViolations()) {
+            message = constraint.getMessage();
+            break;
+        }
         ErrorResult errorResult = new ErrorResult(message);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResult);
     }
